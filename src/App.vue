0f21a385e58e9b17542cfa447e3d330affe5c5b1
ronething <13414113153@163.com>
2018-12-27 10:30:06 +0800

    rem配置🎸

--- src/App.vue
+++ src/App.vue
@@ -6,18 +6,17 @@
 </template>
 
 <script>
+/* eslint-disable */
 export default {
-  name: 'App'
-}
+  name: "App"
+};
+document.addEventListener("DOMContentLoaded", () => {
+  const html = document.querySelector("html");
+  let fontSize = window.innerWidth / 10;
+  fontSize = fontSize > 50 ? 50 : fontSize;
+  html.style.fontSize = fontSize + "px";
+});
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
