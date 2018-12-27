4885834020f969ee30f601aa7686a42c1f342da6
ronething <13414113153@163.com>
2018-12-27 13:25:28 +0800

    翻页功能📚

--- src/Ebook.vue
+++ src/Ebook.vue
@@ -2,6 +2,11 @@
   <div class="ebook">
     <div class="read-wrapper">
       <div id="read"></div>
+      <div class="mask">
+        <div class="left" @click="prevPage"></div>
+        <div class="center"></div>
+        <div class="right" @click="nextPage"></div>
+      </div>
     </div>
   </div>
 </template>
@@ -21,6 +26,7 @@ export default {
   },
   watch: {},
   methods: {
+    //   电子书渲染
     showEpub() {
       this.book = new Epub(DOWNLOAD_URL);
       this.rendition = this.book.renderTo("read", {
@@ -28,6 +34,17 @@ export default {
         height: window.innerHeight
       });
       this.rendition.display();
+    },
+    // 上一页
+    prevPage() {
+      if (this.rendition) {
+        this.rendition.prev();
+      }
+    },
+    nextPage() {
+      if (this.rendition) {
+        this.rendition.next();
+      }
     }
   },
   components: {}
@@ -36,4 +53,27 @@ export default {
 
 <style lang='scss' scoped>
 @import "assets/styles/global";
+.ebook {
+  position: relative;
+  .read-wrapper {
+    .mask {
+      position: absolute;
+      top: 0;
+      left: 0;
+      z-index: 100;
+      display: flex;
+      width: 100%;
+      height: 100%;
+      .left {
+        flex: 0 0 px2rem(100);
+      }
+      .center {
+        flex: 1;
+      }
+      .right {
+        flex: 0 0 px2rem(100);
+      }
+    }
+  }
+}
 </style>
